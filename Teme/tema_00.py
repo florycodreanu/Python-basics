e7e5efd84416b6149e8534bc15516207ff171b28
Flori <florycodreanu@gmail.com>
2023-03-11 15:26:11 +0200

    prima tema

---
+++ Teme/tema_00.py
@@ -0,0 +1,19 @@
+# Exercitiul 2
+
+# variabila string memoreaza siruri de caractere
+
+# Exercitiul 4
+
+nume = 'Flori'
+varsta = 26
+greutate = 52.5
+mamica = True
+print(f'Ma numesc : {nume}')
+print(f'Am implinit {varsta} ani')
+print(f'Cantaresc {greutate} kg')
+print(f'Sunt parinte : {mamica}')
+
+# Ex 6. Numaram de la 1 din 3 in 3 pana ajungem la o valoare mai mica de 20
+
+for i in range(1, 20, 3):
+    print(i)
