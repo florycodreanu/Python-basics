4cddb03b1cdcb851af57243ca3c0d2f40d50d90e
Flori <florycodreanu@gmail.com>
2023-03-14 14:53:15 +0200

    up curs 8

--- Curs/curs_08.py
+++ Curs/curs_08.py
@@ -23,8 +23,8 @@ chrome = webdriver.Chrome()
 time.sleep(3)
 chrome.maximize_window()
 chrome.get("https://formy-project.herokuapp.com/form")  # putem deschide un site
-chrome.find_element(By.ID, "first-name").send_keys("Marian")
-chrome.find_element(By.ID, "last-name").send_keys("Gheorghisor")
+chrome.find_element(By.ID, "first-name").send_keys("Florentina")
+chrome.find_element(By.ID, "last-name").send_keys("Codreanu")
 time.sleep(3)
 chrome.quit()  # inchide toata instanta browserului
 # chrome.close() -> inchide un singur tab (cel activ) din instanta de browser
